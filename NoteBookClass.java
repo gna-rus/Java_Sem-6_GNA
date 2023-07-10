81ba60ceba67748e808cc006c1dfd54983618e04
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-07-10 11:44:28 +0300

    Create NoteBookClass.java

---
+++ NoteBookClass.java
@@ -0,0 +1,107 @@
+import java.util.List;
+
+public class NoteBookClass{
+    private String AllName; // полное название ноутбука
+    private String OpSys; // назвение и версия операционной системы
+    private String color; // цвет ноутбука
+    private int RAM; // обьем ОЗУ
+    private int Hd; // обьем жесткого диска
+    private double weight; // вес ноутбука
+    private double price; // цена ноутбука
+
+  public void NoteBook (String AllName, String OpSys, String color, int RAM,
+			int Hd, double weight, double price)
+  {
+    this.AllName = AllName;
+    this.OpSys = OpSys;
+    this.color = color;
+    this.RAM = RAM;
+    this.Hd = Hd;
+    this.weight = weight;
+    this.price = price;
+  }
+
+  public String GetAllName ()
+  {
+    return AllName;
+  }
+
+  public void SetAllName (String AllName)
+  {
+    this.AllName = AllName;
+  }
+  
+  ////
+  public String GetOpSys ()
+  {
+    return OpSys;
+  }
+
+  public void SetOpSys (String OpSys)
+  {
+    this.OpSys = OpSys;
+  }
+  ////
+  
+  public String getColor()
+  {
+    return color;
+  }
+  
+  public void setColor (String color)
+  {
+    this.color = color;
+  }
+  ////
+  
+   public int getRAM()
+  {
+    return RAM;
+  }
+  
+  public void setRAM (int RAM)
+  {
+    this.RAM = RAM;
+  }
+  ////
+  
+  public int getHd()
+  {
+    return Hd;
+  }
+  
+  public void setHd (int Hd)
+  {
+    this.Hd = Hd;
+  }
+  ////
+  
+  public double getWeight()
+  {
+    return weight;
+  }
+  
+  public void setWeight (double weight)
+  {
+    this.weight = weight;
+  }
+  ////
+  
+  public double getPrice()
+  {
+    return price;
+  }
+  
+  public void setPrice (double price)
+  {
+    this.price = price;
+  }  
+  
+
+
+
+
+
+
+
+}
